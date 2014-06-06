2d9c2728bb1891662c1efb8ffc8b8683c57dfca3
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-05 23:10:22 -0400

    basics of a module for msg creation/serialization

---
+++ erlang/src/rm_msg.erl
@@ -0,0 +1,18 @@
+-module(rm_msg).
+
+-export([new/0, new/1, set/3, get/2, serialize/1, deserialize/1]).
+
+
+-define(STD, 10).
+-define(KA,  0).
+
+new() -> #{}.
+new(Type) when is_atom(Type) -> #{type => Type}.
+
+set(Msg, Field, Value) when is_map(Msg) -> map:put(Field, Value, Msg).
+
+get(Msg, Field) when is_map(Msg) -> map:get(Field, Msg).
+
+serialize(Msg) when is_map(Msg) -> <<>>.
+
+deserialize(Binary) when is_binary(Binary) -> #{}.
