986e17badcdf568a32aaef152ea0a6c4ac27d62b
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-06 20:33:27 -0400

    fill out some protocol filed definitions

--- erlang/src/rm_msg.erl
+++ erlang/src/rm_msg.erl
@@ -3,8 +3,15 @@
 -export([new/0, new/1, set/3, get/2, serialize/1, deserialize/1]).
 
 
--define(STD, 10).
+-define(STD, 1).
 -define(KA,  0).
+
+-define(SYN,    1).
+-define(SYNACK, 2).
+-define(ACK,    3).
+-define(RESET,  4).
+
+-define(Fields, [{type, 1, ?STD}, {phase, 4, ?SYN}, {seq, 3, 0}, {id, 128, 0}, {body, 0, <<>>}]).
 
 new() -> #{}.
 new(Type) when is_atom(Type) -> #{type => Type}.
