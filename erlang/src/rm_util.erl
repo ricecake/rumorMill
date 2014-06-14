18b17e05c9d7eadf910e519a209e54eaa1b51a85
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-13 23:12:27 -0400

    add xor metric distance function

--- erlang/src/rm_util.erl
+++ erlang/src/rm_util.erl
@@ -1,12 +1,17 @@
 -module(rm_util).
 
--export([encodeDocket/1, decodeDocket/1]).
+-export([encodeDocket/1, decodeDocket/1, distance/2]).
 
 encodeDocket(RoutingList) ->
         << <<(encItem(Recipient, Status))/bits >>
                 || {Recipient, Status}<- RoutingList >>.
 
 decodeDocket(Docket) when is_binary(Docket) -> [ decItem(Item) || <<Item:33>> <= Docket].
+
+distance(A, B) when is_binary(B) -> distance(A, binary:encode_unsigned(B));
+distance(A, B) when is_binary(A) -> distance(B, binary:encode_unsigned(A));
+distance(A, B) -> A bxor B.
+
 encItem(Rec, false)-> <<Rec:32/bits, 0:1>>;
 encItem(Rec, true)->  <<Rec:32/bits, 1:1>>.
 
