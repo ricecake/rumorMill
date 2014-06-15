2c4da70e383341abfbd96e21f8e363453547cb7f
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-14 23:52:07 -0400

    add a function to list the buckets that some identifier goes into relative to current node

--- erlang/src/rm_util.erl
+++ erlang/src/rm_util.erl
@@ -1,6 +1,6 @@
 -module(rm_util).
 
--export([encodeDocket/1, decodeDocket/1, distance/2]).
+-export([encodeDocket/1, decodeDocket/1, distance/2, bucketize/2]).
 
 encodeDocket(RoutingList) ->
         << <<(encItem(Recipient, Status))/bits >>
@@ -11,6 +11,12 @@ decodeDocket(Docket) when is_binary(Docket) -> [ decItem(Item) || <<Item:33>> <=
 distance(A, B) when is_binary(B) -> distance(A, binary:encode_unsigned(B));
 distance(A, B) when is_binary(A) -> distance(B, binary:encode_unsigned(A));
 distance(A, B) -> A bxor B.
+
+bucketize(Me, Them) ->
+        MyBytes = [ Byte || <<Byte:8/bits>> <= Me],
+        TheirBytes = [ Byte || <<Byte:8/bits>> <= Them],
+        Buckets = lists:seq(1, length(MyBytes)),
+        [ Bucket || {Bucket, MyB, ThemB} <- lists:zip3(Buckets, MyBytes, TheirBytes), MyB /= ThemB].
 
 encItem(Rec, false)-> <<Rec:32/bits, 0:1>>;
 encItem(Rec, true)->  <<Rec:32/bits, 1:1>>.
